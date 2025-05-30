ea8b6663a3638cdb1f567bd539d98b9021ed52e2
NostGore <nostgore@gmail.com>
2025-05-30 16:53:51 -0500

    dwa

--- mediaDB.js
+++ mediaDB.js
@@ -761,14 +761,14 @@ const mediaDB = [
     portada: "https://files.catbox.moe/za8l90.png",
     video: "https://files.catbox.moe/7dm7ug.mp4",
     autor: "perritogore",
-    categoria: "PELEAS",
+    categoria: "CONFLICTOS",
   },
   {
     titulo: "Lo machetearon y cortazon la mano",
     portada: "https://files.catbox.moe/rl4qtc.png",
     video: "https://files.catbox.moe/r81nkv.mp4",
     autor: "perritogore",
-    categoria: "PELEAS",
+    categoria: "CONFLICTOS",
   },
   {
     titulo: "",
